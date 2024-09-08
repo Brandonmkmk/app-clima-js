fca94ba29a78b3c96e42ba6aab4e4e7f9c23757c
Brandonmkmk <brandonxd611@gmail.com>
2024-09-08 17:09:55 -0600

    add responsive design

--- app.js
+++ app.js
@@ -24,29 +24,21 @@ async function getClima(city){
   container.innerHTML = `
   <h2>Cuidad: ${city}</h2>
   <h2>Temperatura: ${Math.floor(celcius)}</h2>
-  <h2></h2>
+  <h2>Cielo: ${weather}</h2>
   `
+  container.style.backgroundColor = 'rgba(80, 80, 80, 0.8)'
+  container.style.borderRadius = '10px'
+  container.style.color = 'white'
+  container.style.backgroundColor = 'rgba(80, 80, 80, 0.8)'
+  container.style.backgroundColor = 'rgba(80, 80, 80, 0.8)'
   // city.innerHTML = `Cuidad: ${name}`
   // temperature.innerHTML = `Temperatura: ${Math.floor(kelvinToCelcius)}`
-  if(celcius >= 11 &&  celcius <=30){
-    body.style.backgroundImage = `url(https://images.unsplash.com/photo-1606170034961-ee40e2dbe6bf?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xpbWElMjBzb2xlYWRvfGVufDB8fDB8fHww)`
-    body.style.backgroundSize = "cover"; // Ajusta la imagen para cubrir todo el fondo
-    body.style.backgroundRepeat = "no-repeat";
-  }else if(celcius >=31 && celcius <= 40){
-    body.style.backgroundImage = `url(https://www.24morelos.com/content/images/wp-content/uploads/2018/05/calor-sol-caliente-clima-temperatura-termometro.jpg)`
-    body.style.backgroundSize = "cover"; // Ajusta la imagen para cubrir todo el fondo
-    body.style.backgroundRepeat = "no-repeat";
-  }else if(celcius <=10){
-    body.style.backgroundImage = `url(assets/cuidad_fria.jpg)`
-    body.style.backgroundSize = "cover"; // Ajusta la imagen para cubrir todo el fondo
-    body.style.backgroundRepeat = "no-repeat";
-  }
 }
 
 btnSearch.addEventListener('click',async ()=>{   
  await getClima(cityName.value)
  cityName.value = ''
- weatherIcon.style.display = 'none'
+//  weatherIcon.style.display = 'none'
 })
 
 
